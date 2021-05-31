01506849c704fe00043dac5e53cc96588a0b8073
Eduardo Margulis <edumarg@gmx.com>
2021-05-31 09:08:35 +0300

    added mock data for login

---
+++ users.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+export const users = [
+  {
+    userName: "edumarg",
+    email: "edumarg@email.com",
+    firstName: "Edu",
+    lastName: "Marg",
+    city: "Tlv",
+    country: "Israel",
+    postalCode: "11111"
+  },
+  {
+    userName: "harry",
+    email: "harry@email.com",
+    firstName: "harry",
+    lastName: "P",
+    city: "London",
+    country: "England",
+    postalCode: "2222"
+  },
+  {
+    userName: "mickey",
+    email: "mickey@email.com",
+    firstName: "mickey",
+    lastName: "M",
+    city: "Orlandp",
+    country: "USA",
+    postalCode: "333"
+  }
+];
+
+export const RegisterUser = newUser => {
+  for (user of users) {
+    if (newUser.userName === user.userName) {
+      console.log("user name already exists");
+      return;
+    }
+    if (newUser.email === user.email) {
+      console.log("email already register");
+      return;
+    }
+  }
+  users.push(newUser);
+};
