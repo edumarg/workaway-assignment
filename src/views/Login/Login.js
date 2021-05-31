c58f7022eececcbda1e82f8f66db12896e780427
Eduardo Margulis <edumarg@gmx.com>
2021-05-31 14:06:58 +0300

    built login and register forms

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,10 +1,49 @@
 import React, { Component } from "react";
 
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+
 class Login extends Component {
   render() {
+    const myStyle = {
+      textAlign: "center",
+      width: "80%",
+      margin: "7rem auto"
+    };
+
     return (
       <React.Fragment>
-        <h2>Login</h2>
+        <div style={myStyle}>
+          <h2>Login</h2>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              <CustomInput
+                labelText="Username"
+                id="username"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+            <GridItem xs={12} sm={12} md={6}>
+              <CustomInput
+                labelText="Password"
+                id="password"
+                inputProps={{ type: "password" }}
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+          </GridContainer>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={12}>
+              <Button color="primary">Login</Button>
+            </GridItem>
+          </GridContainer>
+        </div>
       </React.Fragment>
     );
   }
--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -1,8 +1,111 @@
 import React, { Component } from "react";
 
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import Button from "components/CustomButtons/Button.js";
+
 class Register extends Component {
   render() {
-    return <h2>Register</h2>;
+    const myStyle = {
+      textAlign: "center",
+      width: "80%",
+      margin: "7rem auto"
+    };
+
+    return (
+      <React.Fragment>
+        <div style={myStyle}>
+          <h2>Register</h2>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={5}>
+              <CustomInput
+                labelText="Company (disabled)"
+                id="company-disabled"
+                formControlProps={{
+                  fullWidth: true
+                }}
+                inputProps={{
+                  disabled: true
+                }}
+              />
+            </GridItem>
+            <GridItem xs={12} sm={12} md={3}>
+              <CustomInput
+                labelText="Username"
+                id="username"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+            <GridItem xs={12} sm={12} md={4}>
+              <CustomInput
+                labelText="Email address"
+                id="email-address"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+          </GridContainer>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              <CustomInput
+                labelText="First Name"
+                id="first-name"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+            <GridItem xs={12} sm={12} md={6}>
+              <CustomInput
+                labelText="Last Name"
+                id="last-name"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+          </GridContainer>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={4}>
+              <CustomInput
+                labelText="City"
+                id="city"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+            <GridItem xs={12} sm={12} md={4}>
+              <CustomInput
+                labelText="Country"
+                id="country"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+            <GridItem xs={12} sm={12} md={4}>
+              <CustomInput
+                labelText="Postal Code"
+                id="postal-code"
+                formControlProps={{
+                  fullWidth: true
+                }}
+              />
+            </GridItem>
+          </GridContainer>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={12}>
+              <Button color="primary">Register User</Button>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
