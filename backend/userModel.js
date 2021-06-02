e554c5edcd1dd9a33b488dcd80841c0518968e77
Eduardo Margulis <edumarg@gmx.com>
2021-06-02 18:52:32 +0300

    added try catch logic to axios commands

--- backend/userModel.js
+++ backend/userModel.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 // Mongoose user Schema
 const userSchema = new mongoose.Schema({
-  userName: { type: String, unique: true, minLength: 3, maxLength: 50 },
-  firstName: { type: String, minLength: 3, maxLength: 50 },
-  lastName: { type: String, minLength: 3, maxLength: 50 },
-  email: { type: String, unique: true, minLength: 3, maxLength: 150 },
-  password: { type: String, maxLength: 255 },
-  city: { type: String, minLength: 3, maxLength: 50 },
-  country: { type: String, minLength: 3, maxLength: 50 },
-  postalCode: { type: String, minLength: 3, maxLength: 50 },
+  userName: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  city: String,
+  country: String,
+  postalCode: String,
 });
 
 // Mongoose user Model
