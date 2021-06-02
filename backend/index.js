2cc566bf13dbb9ff79008ab115e8b6b1e99e78c4
Eduardo Margulis <edumarg@gmx.com>
2021-06-02 10:59:45 +0300

    started server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("node test");
+var express = require("express");
+var cors = require("cors");
+var app = express();
+
+app.use(cors());
+
+app.get("/api/test", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(9000, () => console.log("listening on port 9000..."));
