ec8113f887d17ef93c6a5f1fb83cc55cabfcef60
Eduardo Margulis <edumarg@gmx.com>
2021-06-02 16:05:41 +0300

    added has to passwords

--- backend/userRoute.js
+++ backend/userRoute.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const app = express();
+const bcrypt = require("bcrypt");
 
+const app = express();
 const User = require("./userModel");
 
 app.post("/api/login", async (req, res) => {
   const { userName, password } = req.body;
-  let user = await User.findOne({ userName: userName, password: password });
+
+  let user = await User.findOne({
+    userName: userName,
+  });
   if (!user) {
     return res.status(400).send("Invalid username or password.");
   }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
   res.send(user);
 });
 
@@ -22,9 +29,10 @@ app.post("/api/register", async (req, res) => {
   if (user) return res.status(400).send("user name already registered.");
 
   const newUser = new User(body);
-  console.log("New user to register...");
+  const salt = await bcrypt.genSalt(10);
+  newUser.password = await bcrypt.hash(newUser.password, salt);
+
   const result = await newUser.save();
-  console.log("new user registered", result);
   res.send(result);
 });
 
